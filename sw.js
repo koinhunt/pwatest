62b88287bd2c9a8e1504879057a35212c7d5ac56
koinhunter <koinhunterxh@gmail.com>
2020-03-25 14:34:55 +0800

    add

--- sw.js
+++ sw.js
@@ -1,84 +1,59 @@
-/* membuat variabel untuk nama cache 
-callback dan memutuskan file mana yang akan disimpan 
-dialam cache tersebut 
-*/
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/fallback.json',
-  '/css/main.css',
-  '/images/logo.png',
-  '/js/jquery.min.js',
-  '/js/main.js'
+const nama_cache = 'cache-pwa-v1';
+const aset = [
+    '/',
+    '/index.html',
+    '/pages/home.html',
+    '/pages/about.html',
+    '/pages/product.html',
+    '/pages/contact.html',
+    '/js/main.js',
+    '/js/materialize.js',
+    '/js/materialize.min.js',
+    '/css/materialize.css',
+    '/css/materialize.min.css',
+    '/css/style.css',
+    '/images/logo.png',
+    '/images/aset/medsos_facebook.png',
+    '/images/aset/medsos_instagram.png',
+    '/images/aset/medsos_line.png',
+    '/images/aset/medsos_twitter.png',
+    '/images/aset/banner2.png',
+    '/images/aset/pwa.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
 ];
 
-// implementasi cache then network
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('in install service worker... cache opened!!');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-
-
-//menyimpan cache dan mengembalikan permintaan
-self.addEventListener('fetch', function(event) {
-
-  // var 
-  var request = event.request
-  var url     = new URL(request.url)
-
-  // pisahkan request cache API dan internal
-  if(url.origin === location.origin) {
-    // jika sama
-     event.respondWith(
-      // menampilkan apakah di request ada atau tidak 
-      // kalau ada , maka akan mengembalikan ke response
-      // kalau tidak ada maka akan mengembalikan ke fetch(request)
-      caches.match(request).then(function(response){
-          return response || fetch(request)
-      })
-    )
-  } // jika url nya tidak sama dengan url origin/API tidak sama maka
-    else {
-      event.respondWith(
-          caches.open('products-cache').then(function(cache){
-            return fetch(request).then(function(liveResponse){
-              // menyimpan data ke cache , menggunakan metode .clone untuk cache 1x
-              cache.put(event.request, liveResponse.clone())
-              return liveResponse
-            }).catch(function(){
-              return caches.match(request).then(function(response){
-                if(response) return response
-                  return caches.match('/fallback.json')
-              })
-            })
-          })
-        )
-  }
-});
-
-
-
-
-// Aktivasi dan atau mengupdate serviceworker
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        // filter bila bukan nama cache yang sebelumnya
-        cacheNames.filter(function(cacheName){
-          return cacheName != CACHE_NAME
-        }).map(function(cacheName){
-          return caches.delete(cacheName)
+//install service worker
+self.addEventListener('install', (event) => {
+    // menunggu me load semua aset sebelum mengintal
+    event.waitUntil(
+        caches.open(nama_cache).then(cache => {
+            console.log('mengambil aset');
+            cache.addAll(aset);
         })
-      );
-    })
-  );
+    );
 });
+
+
+// fetching servis worker
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(respon => {
+            return respon || fetch(event.request);
+        })
+    );
+});
+
+
+//aktivasi servis worker
+self.addEventListener('activate', (event) => {
+    // mengganti cache lama dengan yang baru (update)
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(k => k !== nama_cache)
+                .map(k => caches.delete(k))
+            )
+        })
+    );
+});
+
